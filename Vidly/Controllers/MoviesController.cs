94d342494c5a3d703c6a63a4878518f2013e1622
Tom Quinn <tomquinn@hotmail.co.nz>
2017-11-13 17:39:34 +1300

    Merged Vidly with PersonalWebsite repo.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random (will re-implement later).
+        /*public ActionResult Random()
+        {
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie
+            };
+            return View(viewModel);
+        }
+        */
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies base
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var movie = GetMovies().SingleOrDefault(mov => mov.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(movie);
+            }
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-E"}
+            };
+        }
+
+    }
+}
