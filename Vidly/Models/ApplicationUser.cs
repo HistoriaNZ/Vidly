58ad7830582a4f0510771f5830126ebb4a15d399
Tom Quinn <tomquinn@hotmail.co.nz>
2017-11-19 17:01:16 +1300

    Refactors identity models into separate classes.

--- Vidly/Models/ApplicationUser.cs
+++ Vidly/Models/ApplicationUser.cs
@@ -26,23 +26,4 @@ namespace Vidly.Models
             return userIdentity;
         }
     }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        //Initialise tables
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<Genre> Genre { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
