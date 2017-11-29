adb56fd732712b8b394dfc8f0e764b2c567050eb
Tom Quinn <tomquinn@hotmail.co.nz>
2017-11-29 14:27:07 +1300

    Adds basic returns view and adds link to the nav bar.

---
+++ Vidly/ViewModels/ReturnViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.ViewModels
+{
+    public class ReturnViewModel
+    {
+        [Required]
+        public DateTime DateRented { get; set; }
+
+        public DateTime? DateReturned { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int MovieId { get; set; }
+    }
+}
