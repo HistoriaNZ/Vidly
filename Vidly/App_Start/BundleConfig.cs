a053a11236bc3ee0b6db64f0ff2550e1b8b7ff20
Tom Quinn <tomquinn@hotmail.co.nz>
2017-11-29 16:21:40 +1300

    Adds Ajax call to rentals API based on the customer selected (returns view).

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace Vidly
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
